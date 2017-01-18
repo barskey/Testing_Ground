5701e1ff69b081c1fbaa6522e09806e6acd8ecb4
Steve Barskey <steve@Steves-iMac.local>
2017-01-17 21:37:48 -0800

    After Lecture 209. C++ AI Behavior Tree Tasks.

--- Source/Testing_Ground/Testing_Ground.Build.cs
+++ Source/Testing_Ground/Testing_Ground.Build.cs
@@ -6,6 +6,6 @@ public class Testing_Ground : ModuleRules
 {
 	public Testing_Ground(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 	}
 }
