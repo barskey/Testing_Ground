32d2abf9fa090503c1c8cbf90de0968888f0f52f
Steve Barskey <steve@Steves-iMac.local>
2017-01-25 22:01:48 -0800

    After Lecture 244. Another major change to lfs tracked files. Created top-level Static for gitlfs and Dynamic for regular git tracking.

--- Source/Testing_Ground/Testing_GroundHUD.cpp
+++ Source/Testing_Ground/Testing_GroundHUD.cpp
@@ -9,7 +9,7 @@
 ATesting_GroundHUD::ATesting_GroundHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
