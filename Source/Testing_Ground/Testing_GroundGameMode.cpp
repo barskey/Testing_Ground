37720be74fd2e2818f923f7cf43a80be3d2c5342
Steve Barskey <steve@Steves-iMac.local>
2017-02-02 23:07:40 -0800

    After Lecture 264. Merging our TP and FP Files.

--- Source/Testing_Ground/Testing_GroundGameMode.cpp
+++ Source/Testing_Ground/Testing_GroundGameMode.cpp
@@ -9,7 +9,7 @@ ATesting_GroundGameMode::ATesting_GroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
